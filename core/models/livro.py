505bb37398536a69ae3466001f5da1a52b098c75
victor-de-sousa <victordesousaestudo@gmail.com>
2026-03-17 16:32:15 -0300

    feat: include many to many relationship between Livro and Autor

--- core/models/livro.py
+++ core/models/livro.py
@@ -1,7 +1,6 @@
 from django.db import models
 
-from .categoria import Categoria
-from .editora import Editora
+from core.models import Autor, Categoria, Editora 
 
 class Livro(models.Model):
     titulo = models.CharField(max_length=255)
@@ -10,6 +9,7 @@ class Livro(models.Model):
     preco = models.DecimalField(max_digits=7, decimal_places=2, default=0)
     categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name='livros', null=True, blank=True)
     editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name='livros', null=True, blank=True)
+    autores = models.ManyToManyField(Autor, related_name='livros', blank=True)
 
     def __str__(self):
         return f'({self.id}) {self.titulo} ({self.quantidade})'
