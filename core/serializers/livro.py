5be0bbb30f3fde86a2aa775267e0c7d2e0ef7a8d
victor-de-sousa <victordesousaestudo@gmail.com>
2026-04-07 09:17:00 -0300

    feat: multiple serializers to Livro

--- core/serializers/livro.py
+++ core/serializers/livro.py
@@ -2,12 +2,16 @@ from rest_framework.serializers import ModelSerializer
 
 from core.models import Livro
 
-class LivroListRetrieveSerializer(ModelSerializer):
+class LivroListSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = ('id', 'titulo', 'preco')
+        
+class LivroRetrieveSerializer(ModelSerializer):
     class Meta:
         model = Livro
         fields = '__all__'
         depth = 1
-        
 
 class LivroSerializer(ModelSerializer):
     class Meta:
