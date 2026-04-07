ac19c3791d115e08e8c3f0d83d440cb59d9c2e48
victor-de-sousa <victordesousaestudo@gmail.com>
2026-04-07 09:07:08 -0300

    feat: two serializers to Livro

--- core/models/livro.py
+++ core/models/livro.py
@@ -9,7 +9,7 @@ class Livro(models.Model):
     preco = models.DecimalField(max_digits=7, decimal_places=2, default=0)
     categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name='livros', null=True, blank=True)
     editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name='livros', null=True, blank=True)
-    autores = models.ManyToManyField(Autor, related_name='livros', blank=True)
+    autores = models.ManyToManyField(Autor, related_name='livros', blank=True, null=True)
 
     def __str__(self):
         return f'({self.id}) {self.titulo} ({self.quantidade})'
--- core/serializers/livro.py
+++ core/serializers/livro.py
@@ -2,6 +2,13 @@ from rest_framework.serializers import ModelSerializer
 
 from core.models import Livro
 
+class LivroListRetrieveSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = '__all__'
+        depth = 1
+        
+
 class LivroSerializer(ModelSerializer):
     class Meta:
         model = Livro
