13b9c92d7e953f7a836eb415e7a2e84c4b555895
victor-de-sousa <victordesousaestudo@gmail.com>
2026-04-14 16:03:50 -0300

    feat: dump and load data

--- core/models/editora.py
+++ core/models/editora.py
@@ -1,9 +1,10 @@
 from django.db import models
 
-
 class Editora(models.Model):
     nome = models.CharField(max_length=100)
     site = models.URLField(max_length=200, null=True, blank=True)
+    email = models.EmailField(max_length=100, blank=True, null=True)
+    cidade = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return f'{self.nome}'
