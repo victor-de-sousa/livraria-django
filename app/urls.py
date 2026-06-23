88fe2d3791e1e179da92bbd5ac439bdd99d5b649
victor-de-sousa <victordesousaestudo@gmail.com>
2026-06-23 15:49:44 -0300

    feat: create basic purchase listing endpoint

--- app/urls.py
+++ app/urls.py
@@ -19,6 +19,7 @@ from rest_framework_simplejwt.views import (
 from core.views import (
     AutorViewSet,
     CategoriaViewSet,
+    CompraViewSet,
     EditoraViewSet,
     LivroViewSet,
     UserRegistrationView,
@@ -32,6 +33,7 @@ router = DefaultRouter()
 router.register(r'usuarios', UserViewSet, basename='usuarios')
 router.register(r'autores', AutorViewSet, basename='autores')
 router.register(r'categorias', CategoriaViewSet, basename='categorias')
+router.register(r'compras', CompraViewSet, basename='compras')
 router.register(r'editoras', EditoraViewSet, basename='editoras')
 router.register(r'livros', LivroViewSet, basename='livros')
 
--- core/views/__init__.py
+++ core/views/__init__.py
@@ -1,5 +1,6 @@
 from .autor import AutorViewSet
 from .categoria import CategoriaViewSet
+from .compra import CompraViewSet
 from .editora import EditoraViewSet
 from .livro import LivroViewSet
 from .user import UserRegistrationView, UserViewSet
