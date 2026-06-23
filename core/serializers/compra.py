466c4546c97246dc1d8209add1243f6d1f38623f
victor-de-sousa <victordesousaestudo@gmail.com>
2026-06-23 15:54:24 -0300

    feat: include purchase status in purchase listing

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -4,6 +4,9 @@ from core.models import Compra
 
 class CompraSerializer(ModelSerializer):
     usuario = CharField(source='usuario.email', read_only=True)
+    status = CharField(source='get_status_display', read_only=True)
+
+
     class Meta:
         model = Compra
         fields = '__all__'
