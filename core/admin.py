d2dee6eca3832a8e5bc6c17a3fdafde9829276bf
victor-de-sousa <victordesousaestudo@gmail.com>
2026-07-07 08:35:40 -0300

    feat: add purchase total to Admin

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,7 @@
 """
 Django admin customization.
 """
-from django.contrib.admin import ModelAdmin, register, TabularInline
+from django.contrib.admin import ModelAdmin, register, TabularInline, display
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 
@@ -30,12 +30,18 @@ class ItensCompraInline(TabularInline):
 
 @register(Compra)
 class CompraAdmin(ModelAdmin):
-    list_display = ('usuario', 'status')
+    list_display = ('usuario', 'status', 'total_formatado')
     search_fields = ('usuario', 'status')
     list_filter = ('usuario', 'status')
     ordering = ('usuario', 'status')
     list_per_page = 10
     inlines = [ItensCompraInline]
+    readonly_fields = ("total_formatado",)
+
+    @display(description="Total")
+    def total_formatado(self, obj):
+        """Exibe 123,45 em vez de 123.45."""
+        return f"R$ {obj.total:.2f}"
 
 
 @register(Editora)
