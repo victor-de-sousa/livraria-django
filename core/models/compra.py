c4e4ac185704d92d3fe5b8578e34fd5009559c1d
victor-de-sousa <victordesousaestudo@gmail.com>
2026-09-15 09:29:09 -0300

    feat: add purchase type to entity Compra [REFS:#34]

--- core/models/compra.py
+++ core/models/compra.py
@@ -11,8 +11,22 @@ class Compra(models.Model):
         PAGO = 3, 'Pago'
         ENTREGUE = 4, 'Entregue'
 
+    class TipoPagamento(models.IntegerChoices):
+        CARTAO_CREDITO = 1, 'Cartão de Crédito'
+        CARTAO_DEBITO = 2, 'Cartão de Débito'
+        PIX = 3, 'PIX'
+        BOLETO = 4, 'Boleto'
+        TRANSFERENCIA_BANCARIA = 5, 'Transferência Bancária'
+        DINHEIRO = 6, 'Dinheiro'
+        CHEQUE = 7, 'Cheque'
+        OUTRO = 8, 'Outro'
+
     usuario = models.ForeignKey(User, on_delete=models.PROTECT, related_name='compras')
     status = models.IntegerField(choices=StatusCompra.choices, default=StatusCompra.CARRINHO)
+    tipo_pagamento = models.IntegerField(
+        choices=TipoPagamento.choices,
+        default=TipoPagamento.PIX
+    )
     data = models.DateTimeField(auto_now_add=True)
 
     @property
--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -98,8 +98,9 @@ class CompraSerializer(ModelSerializer):
     usuario = CharField(source='usuario.email', read_only=True)
     status = CharField(source='get_status_display', read_only=True)
     data=DateTimeField(read_only=True)
+    tipo_pagamento = CharField(source='get_tipo_pagamento_display', read_only=True)
     itens = ItensCompraSerializer(many = True, read_only = True)
 
     class Meta:
         model = Compra
-        fields = ('id', 'usuario', 'status', 'total', 'data', 'itens')
+        fields = ('id', 'usuario', 'status', 'tipo_pagamento', 'total', 'data', 'itens')
